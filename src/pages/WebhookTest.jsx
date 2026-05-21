484a43d98fd4062f98c8129d82562866a8be32ed
Jhonata Emerick <jhonata.emerick@gmail.com>
2026-05-21 09:34:40 +0000

    File changes

--- src/pages/WebhookTest.jsx
+++ src/pages/WebhookTest.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const FILE_URL = "https://media.base44.com/files/public/6a0e7a7ba2f3e7202ba94ab4/f8d15308a_POLITICADEDESPESASEREEMBOLSO.pdf";
+const FILE_URL = "https://media.base44.com/files/public/6a0e7a7ba2f3e7202ba94ab4/ce5642c19_POLITICADEDESPESASEREEMBOLSO.pdf";
 const FILE_NAME = "POLITICADEDESPESASEREEMBOLSO.pdf";
 const WEBHOOK = "https://webhooks.oreembolsobot.app/webhook/01772137-eb84-4ed4-a308-d5dfe1eb06f2";
 
